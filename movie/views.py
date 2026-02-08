e3c4518cd8a27fd5627959709360bf0fa24afee7
Miguel Garcia <miguelangelgarciaosorio42@gmail.com>
2026-02-08 14:35:38 -0500

    Cambios en la funcion Home

--- movie/views.py
+++ movie/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,"home.html",{'name':'Miguel Garcia'});
+    return render(request,"home.html",{'name':'Kanye West'});
 
 def about(request):
     return HttpResponse("Bro");
