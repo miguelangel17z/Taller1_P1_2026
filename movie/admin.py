0eefa7442111d304f588b43ea8cf9bd367449055
Miguel Garcia <miguelangelgarciaosorio42@gmail.com>
2026-02-08 15:42:15 -0500

    Busqueda de peliculas

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import movie
+
+admin.site.register(movie)
 
 # Register your models here.
--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import movie
 
 def home(request):
-    return render(request,"home.html",{'name':'Kanye West'});
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = movie.objects.all()
+    return render(request,"home.html",{'searchTerm': searchTerm, 'movies': movies});
 
 def about(request):
     return render(request,"About.html");
